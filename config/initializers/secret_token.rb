2cc700c7bb94e480604a05eaf8ca0954db4ff749
Marcelo Eduardo de Andrade Jr <marcelinhov2@Marcelos-MacBook-Pro.local>
2012-06-01 16:35:12 -0300

    Hello World Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '84e3a8f582e18c43ec8c71d741ef9cf7396747a4250a0431fe3ee5cfa209ad7bb723e23a674763ecc4b5db2d0f28cbe801a9667c9d2ba1fcb24bc826152174ab'
